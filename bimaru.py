6a540009b43656b943f33665d0be1dc5c31aa425
dudas_duude <duarte.costa01@hotmail.com>
2023-05-27 16:36:18 +0100

    Updates to parse_instance and added a testing instance

--- bimaru.py
+++ bimaru.py
@@ -36,8 +36,6 @@ class BimaruState:
 class Board:
     """Representação interna de um tabuleiro de Bimaru."""
 
-    game_cells = np.empty((10, 10), dtype=np.chararray)
-
     def __init__(self):
         self.row_elements = []
         self.col_elements = []
@@ -46,23 +44,34 @@ class Board:
     def get_value(self, row: int, col: int) -> str:
         """Devolve o valor na respetiva posição do tabuleiro."""
         # TODO
-        return self.game_cells[row][col]
+        return self.game_cells[row, col]
 
     def adjacent_vertical_values(self, row: int, col: int) -> (str, str):
         """Devolve os valores imediatamente acima e abaixo,
         respectivamente."""
         # TODO
-        up_value = self.game_cells[row-1][col]
-        down_value = self.game_cells[row+1][col]
+        up_value = self.game_cells[row - 1, col]
+        down_value = self.game_cells[row + 1, col]
         return up_value, down_value
 
     def adjacent_horizontal_values(self, row: int, col: int) -> (str, str):
         """Devolve os valores imediatamente à esquerda e à direita,
         respectivamente."""
         # TODO
-        right_value = self.game_cells[row][col+1]
-        left_value = self.game_cells[row][col-1]
-        return right_value, left_value
+        right_value = self.game_cells[row, col + 1]
+        left_value = self.game_cells[row, col - 1]
+        return left_value, right_value
+
+    def adjacent_diagonal_values(self, row: int, col: int) -> (str, str, str, str):
+        """Devolve os valores das diagonais adjacentes da célula"""
+
+        # TODO WILL THIS BE NEEDED?
+
+        r_up = self.game_cells[row - 1, col + 1]
+        r_down = self.game_cells[row + 1, col - 1]
+        l_up = self.game_cells[row - 1, col + 1]
+        l_down = self.game_cells[row + 1, col + 1]
+        return r_up, r_down, l_up, l_down
 
     @staticmethod
     def parse_instance():
@@ -76,7 +85,7 @@ class Board:
             > line = stdin.readline().split()
         """
         # TODO
-        initial_board = Board
+        initial_board = Board()
 
         # Gets the number of boats elements per column/row
         for i in range(2):
@@ -105,16 +114,39 @@ class Board:
                 print("Problem with HINT!")
                 return None
 
+        initial_board.water_fill()
+
         return initial_board
 
     # TODO: outros metodos da classe
+
+    # Used to fill with water in columns/lines where no more boats can be put
+    def water_fill(self):
+
+        # for lines
+        for i in range(10):
+            # for row
+            filled_cells = np.count_nonzero(self.game_cells[i, :])
+            elements = int(self.row_elements[i])
+            water_tips = np.count_nonzero(np.logical_or(self.game_cells[i, :] == 'W', self.game_cells[i, :] == '.'))
+            if filled_cells - elements - water_tips == 0:
+                self.game_cells[i, :] = ['.' if cell is None else cell for cell in self.game_cells[i, :]]
+
+            # for column
+            filled_cells = np.count_nonzero(self.game_cells[:, i])
+            elements = int(self.col_elements[i])
+            water_tips = np.count_nonzero(np.logical_or(self.game_cells[:, i] == 'W', self.game_cells[:, i] == '.'))
+            if filled_cells - elements - water_tips == 0:
+                self.game_cells[:, i] = ['.' if cell is None else cell for cell in self.game_cells[:, i]]
+
+        # TODO
 
 
 class Bimaru(Problem):
     def __init__(self, board: Board):
         """O construtor especifica o estado inicial."""
         # TODO
-        pass
+        self.board = board
 
     def actions(self, state: BimaruState):
         """Retorna uma lista de ações que podem ser executadas a
@@ -128,14 +160,20 @@ class Bimaru(Problem):
         das presentes na lista obtida pela execução de
         self.actions(state)."""
         # TODO
-        pass
+
+        state.board.game_cells[action[0], action[1]] = action[2]
+
+        return BimaruState
 
     def goal_test(self, state: BimaruState):
         """Retorna True se e só se o estado passado como argumento é
         um estado objetivo. Deve verificar se todas as posições do tabuleiro
         estão preenchidas de acordo com as regras do problema."""
         # TODO
-        pass
+
+        if self.board.game_cells.size - np.count_nonzero(self.board.game_cells) != 0:
+            return False
+        return True
 
     def h(self, node: Node):
         """Função heuristica utilizada para a procura A*."""
@@ -154,6 +192,19 @@ if __name__ == "__main__":
 
     game_board = Board.parse_instance()
 
-    print('CHECK')
+    problem = Bimaru(game_board)
+
+    s0 = BimaruState(game_board)
+
+    # result_state = problem.result(initial_state, (3, 3, 'w'))
+
+    print(game_board.adjacent_vertical_values(3, 3))
+    print(game_board.adjacent_horizontal_values(3, 3))
+    print(problem.goal_test(s0))
+
+    # Convert array to string where each row is a line
+    string_representation = '\n'.join([''.join(row.astype(str)) for row in game_board.game_cells])
+
+    print(string_representation)
 
     pass
